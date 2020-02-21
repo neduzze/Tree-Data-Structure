b72ddf6248be60cc478e652f5436ec2869cae489
Chinedum Ugo Atuonwu <neduzze@gmail.com>
2020-02-21 22:13:03 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TREE_DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TREE_DataStructure.dir/src/main.cpp.o"
+  "TREE_DataStructure"
+  "TREE_DataStructure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TREE_DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
